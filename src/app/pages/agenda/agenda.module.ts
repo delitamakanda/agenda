d15265ffafb9e14daec6de27454280159676fb98
Délita Makanda <delita.makanda@gmail.com>
2020-04-07 21:19:28 +0200

    init dates

--- src/app/pages/agenda/agenda.module.ts
+++ src/app/pages/agenda/agenda.module.ts
@@ -1,19 +1,18 @@
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AgendaPage } from './agenda.page';
-import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: AgendaPage }]),
-    SharedModule
+    RouterModule.forChild([{ path: '', component: AgendaPage }])
   ],
+  providers: [DatePipe],
   declarations: [AgendaPage]
 })
 export class AgendaPageModule {}
